01d57875a14c7acfbfc5680eb3c404b8895bac2e
root <ofentseloeto>
2023-12-28 17:03:12 +0000

    Basic babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+0-app.py - Basic Flask App
+
+This module defines a basic Flask web application with a
+single route ("/") and an index.html template.
+
+Usage:
+    Run this script to start the Flask development server.
+
+Routes:
+    - /: Renders the index.html template with the title
+         "Welcome to Holberton" and the header "Hello world".
+
+Dependencies:
+    - Flask: The web framework used to create the application.
+
+Author:
+    Ofentse Loeto
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """
+    Route handler for the root ("/") endpoint.
+
+    Returns:
+        str: Rendered HTML content from the index.html template.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
