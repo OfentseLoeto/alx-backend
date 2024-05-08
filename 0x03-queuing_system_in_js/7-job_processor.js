21bc028d81829a1865396b1bf4a994b119ae271f
Ofentse Loeto <ohentseloeto610@gmail.com>
2024-05-08 17:16:44 +0000

    Track progress and errors with Kue: Create the Job processor

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,25 @@
+const kue = require('kue');
+const queue = kue.createQueue();
+
+
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+
+const sendNotification = (phoneNumber, message, job, done) => {
+
+  job.progress(0, 100);
+  
+  if(blacklistedNumbers.includes(phoneNumber)) {
+    return done(new Error(`PhoneNumber ${phoneNumber} is blacklisted`));
+  }
+
+  job.progress(50, 100);
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  done();
+};
+
+queue.process('push_notification_code_2', 2, (job, done) => {
+
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
+});
